00ef45d9ee3d7aabc52c075c18aa159d4f22633d
Jino John <leojino@gmail.com>
2021-09-28 11:24:13 +0530

    Update Intial Page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
-import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Navbar, Row, Col, Button } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <Navbar bg="dark">
+          <Container fluid>
+            <Navbar.Brand>
+              <span className="text-primary">Land Chain Project</span>
+            </Navbar.Brand>
+          </Container>
+        </Navbar>
+        <section className="banner">
+          <Container className="p-4">
+            <Row>
+              <Col sm="8">
+                <h1>Welcome to the Land Chain Project</h1>
+                <p>Link your property to the chain.</p>
+                <hr/>
+                <Button>Get Started</Button>
+              </Col>
+            </Row>
+          </Container>
+        </section>
       </header>
     </div>
   );
